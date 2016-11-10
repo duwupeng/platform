83d86ca3abf2f8b4974604178ba5ac44214bebd1
赵康弘 <409649485@qq.com>
2016-11-10 14:51:27 +0800

    crud demo提交

--- service-a/src/main/java/com/beizhi/cloud/services/a/dao/UserSqlProvider.java
+++ service-a/src/main/java/com/beizhi/cloud/services/a/dao/UserSqlProvider.java
@@ -2,22 +2,39 @@ package com.beizhi.cloud.services.a.dao;
 
 
 import com.beizhi.cloud.services.a.model.User;
+import org.apache.commons.lang.StringUtils;
 import org.apache.ibatis.jdbc.SQL;
 
 /**
  * Created by kanghong.zhao on 2016-11-7.
  */
-public class UserInsertSqlProvider {
+public class UserSqlProvider {
 
     public String buildInsertSql(User user){
         return new SQL(){{
-            INSERT_INTO("userinfo");
+            INSERT_INTO("user");
             VALUES("username", "#{username}");
             VALUES("password", "#{password}");
             if(user.getNameCn() != null && !"".equals(user.getNameCn().trim()))
                 VALUES("name_cn", "#{nameCn}");
             if(user.getNameEn() != null && !"".equals(user.getNameEn().trim()))
                 VALUES("name_en", "#{nameEn}");
+        }}.toString();
+    }
+
+    public String buildUpdateSql(User user){
+        return new SQL(){{
+            UPDATE("user");
+            if(StringUtils.isNotBlank(user.getUsername())){
+                SET("username = #{username}");
+            }
+            if(StringUtils.isNotBlank(user.getNameCn())){
+                SET("name_cn = #{nameCn}");
+            }
+            if(StringUtils.isNotBlank(user.getNameEn())){
+                SET("name_en = #{nameEn}");
+            }
+            WHERE("id = #{id}");
         }}.toString();
     }
 
--- service-a/src/main/java/com/beizhi/cloud/services/a/service/UserService.java
+++ service-a/src/main/java/com/beizhi/cloud/services/a/service/UserService.java
@@ -3,24 +3,46 @@ package com.beizhi.cloud.services.a.service;
 
 import java.util.List;
 
-import com.beizhi.cloud.services.a.dao.UserDao;
+import com.beizhi.cloud.services.a.dao.UserMapper;
 import com.beizhi.cloud.services.a.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class UserService {
 
-	
 	@Autowired
-	private UserDao userMapper;
+	private UserMapper userMapper;
 	
 	public List<User> searchAll(){
 		List<User> list = userMapper.findAll();
 		System.out.println("list: " + list.size());
 		return list;
+	}
+
+	@Transactional(readOnly = false)
+	public User createUser(User user) {
+		userMapper.insert(user);
+		return getUserById(user.getId());
+	}
+
+	public User getUserById(Integer id) {
+		return userMapper.getById(id);
+	}
+
+	@Transactional(readOnly = false)
+	public User updateUser(User user) {
+		userMapper.update(user);
+		return getUserById(user.getId());
+	}
+
+	@Transactional(readOnly = false)
+	public void deleteUser(Integer userId) {
+		userMapper.delete(userId);
 	}
 }
