8eca7c5dbe612ae3eab631523284c385ab96f360
eric <865361183@qq.com>
2016-11-11 13:06:36 +0800

    add common module ddependency

--- service-a/src/main/resources/sql/init.sql
+++ service-a/src/main/resources/sql/init.sql
@@ -1,9 +1,9 @@
 CREATE SCHEMA `demo_schema` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
 
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `t_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `userinfo` (
+CREATE TABLE `t_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '自增长主键',
   `username` varchar(45) COLLATE utf8_bin NOT NULL COMMENT '用户名',
   `born_at` date DEFAULT NULL,
