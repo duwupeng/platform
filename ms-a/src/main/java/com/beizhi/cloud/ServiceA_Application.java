15d4ca94c8193003459e4b040e8d317976f443b1
eric <865361183@qq.com>
2016-11-17 09:27:58 +0800

    add task scheduler feature

--- ms-a/src/main/java/com/beizhi/cloud/ServiceA_Application.java
+++ ms-a/src/main/java/com/beizhi/cloud/ServiceA_Application.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by eric on 16/11/7.
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableScheduling
 public class ServiceA_Application {
     public static void main(String[] args) {
         SpringApplication.run(ServiceA_Application.class, args);
---
+++ ms-a/src/main/java/com/beizhi/cloud/services/a/scheduler/Task.java
@@ -0,0 +1,59 @@
+package com.beizhi.cloud.services.a.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+/**
+ * Created by eric on 16/11/17.
+ */
+@Component
+public class Task {
+    private Logger logger  = LoggerFactory.getLogger(this.getClass());
+
+    @Scheduled(cron = "0,30 * * * * *")
+    public void cronJob(){
+        logger.info(">cronJob");
+        logger.info("There are {} greetings in the data store");
+        logger.info("<cronJob");
+
+    }
+
+    @Scheduled(initialDelay = 5000,
+            fixedRate = 15000)
+    public void fixedRateJobWithInitialDelay(){
+        logger.info(">fixedRateJobWithInitialDelay");
+        long pause = 5000;
+        long start = System.currentTimeMillis();
+        do{
+            if(start + pause < System.currentTimeMillis()){
+                break;
+            }
+        } while (true);
+        logger.info("Processing time was {} seconds.", pause/1000);
+
+        logger.info("<fixedRateJobWithInitialDelay");
+
+    }
+
+    @Scheduled(initialDelay = 5000,
+            fixedDelay= 15000)
+    public void fixedDelayJobWithInitialDelay(){
+        logger.info(">fixedDelayJobWithInitialDelay");
+        long pause = 5000;
+        long start = System.currentTimeMillis();
+        do{
+            if(start + pause < System.currentTimeMillis()){
+                break;
+            }
+        } while (true);
+        logger.info("Processing time was {} seconds.", pause/1000);
+
+        logger.info("<fixedDelayJobWithInitialDelay");
+
+    }
+}
